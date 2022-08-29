fe7ebb30306ba5fe0060c79ac060568005fdc321
m.witzik <m.witzik@B-WRK-0224>
2022-08-29 09:21:04 +0200

    2

--- CarInspection2/Program.cs
+++ CarInspection2/Program.cs
@@ -7,12 +7,19 @@ namespace CarInspection2
         static void Main()
         {
             // Array von Autos
-            // string[] carNames = new string[6];
-            // Array von Cars (von der Klasse)
+            //  string[] carNames = new string[6];
+            //  string[0] = "Mercedes G"
+            //  string[1] = "BMW 3", 
+            //  string[2] = "Audi A6"
+            //  string[3] = "Audi A8"
+            //  string[4] = "Hyundai"
+            //  string[5] = "Toyota"
+
+            // Array mit der Klasse Car
             Car[] cars = new Car[6];
             cars[0] = new Car("Mercedes G", 2008);
             cars[1] = new Car("BMW 3", 2001);
-            cars[2] = new Car("Audi A&", 2005);
+            cars[2] = new Car("Audi A6", 2005);
             cars[3] = new Car("Audi A8", 2012);
             cars[4] = new Car("Hyundai", 2003);
             cars[5] = new Car("Toyota", 1995);
@@ -25,11 +32,13 @@ namespace CarInspection2
 
             Console.WriteLine("Folgende Autos wurden gefunden: ");
 
-            foreach(string carName in carNames)
+            // gilt für Array von Autos
+            //foreach(string carName in cars)
+            foreach(Car car in cars)
             {
-                if (carName.Contains(searchTerm))
+                if (car.carName.Contains(searchTerm))
                 {
-                    Console.WriteLine(carName);
+                    Console.WriteLine(car.carName + " Baujahr:: " + car.carBaujahr);
                 }
 
 
