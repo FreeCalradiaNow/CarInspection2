edace4f9ac880ae16727a85b95f70551dfcaca89
FreeCalradiaNow <111363541+FreeCalradiaNow@users.noreply.github.com>
2022-08-29 08:44:15 +0200

    Add files via upload

--- CarInspection2/Program.cs
+++ CarInspection2/Program.cs
@@ -6,7 +6,32 @@ namespace CarInspection2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Array von Autos
+            string[] carNames = new string[6];
+            carNames[0] = "Mercedes G";
+            carNames[1] = "Audi A8";
+            carNames[2] = "BMW 3";
+            carNames[3] = "Audi A6";
+            carNames[4] = "Hyundai";
+            carNames[5] = "Toyota";
+
+            Console.WriteLine("Bitte geben Sie einen Suchbegriff ein: ");
+
+            string searchTerm = Console.ReadLine();
+
+            Console.WriteLine("Der Suchbegriff ist: " + searchTerm);
+
+            Console.WriteLine("Folgende Autos wurden gefunden: ");
+
+            foreach(string carName in carNames)
+            {
+                if (carName.Contains(searchTerm))
+                {
+                    Console.WriteLine(carName);
+                }
+
+
+            } 
         }
     }
 }
