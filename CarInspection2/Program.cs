04f06bdf6964a2920a420aefc03da647146d601b
FreeCalradiaNow <111363541+FreeCalradiaNow@users.noreply.github.com>
2022-08-29 08:13:45 +0200

    Add files via upload

---
+++ CarInspection2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarInspection2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
